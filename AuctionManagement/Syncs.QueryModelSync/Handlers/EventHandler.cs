01f3fd688936c7393673bb4cf365cfadad31897a
Mehdi Taherabadi <MehdiTaherabadi@outlook.com>
2022-04-15 11:57:47 +0430

    add to git

---
+++ AuctionManagement/Syncs.QueryModelSync/Handlers/EventHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AuctionManagement.Domain.Contracts.Auctions;
+using Sparta.Domain;
+
+namespace Syncs.QueryModelSync.Handlers
+{
+    public class AuctionEventHandler
+    {
+        public void Handle(AuctionOpened @event)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("AuctionOpened Handled !");
+        }
+
+        public void Handle(BidPlaced @event)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("BidPlace Handled !");
+        }
+    }
+}
---
+++ AuctionManagement/src/AuctionManagement.Domain/Model/Auctions/Auction_EventHandlers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AuctionManagement.Domain.Contracts.Auctions;
+using Sparta.Core.EventHandling;
+using Sparta.Domain;
+
+namespace AuctionManagement.Domain.Model.Auctions
+{
+    public partial class Auction 
+    {
+        public void Causes(DomainEvent @event)
+        {
+            Apply((dynamic)@event);
+            Publish(@event);
+        }
+        public override void Apply(DomainEvent @event)
+        {
+            this.When((dynamic)@event);
+        }
+        private void When(AuctionOpened @event)
+        {
+            this.Id = @event.Id;
+            this.EndDateTime = @event.EndDateTime;
+            this.Product = new SellingProduct(@event.ProductCategoryId, @event.ProductName);
+            this.SellerId = @event.SellerId;
+            this.StartingPrice = @event.StartingPrice;
+        }
+        private void When(BidPlaced @event)
+        {
+            this.WinningBid = new Bid(@event.BidderId, @event.Amount, @event.EventCreateDateTime);
+        }
+    }
+}
